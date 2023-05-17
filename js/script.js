be25c276e05f779a7e430efd4d635bac10be02fe
Krzysztof Marcinkiewicz <marcinkiewiczkrzysiek@gmail.com>
2023-05-17 19:25:28 +0200

    Add numbers and computerMove to script.js

--- js/script.js
+++ js/script.js
@@ -12,5 +12,23 @@ var kamie_C5_84 = "kamień";
 
 var playerMove = "papier";
 printMessage(
-  "Zagrałem " + kamie_C5_84 + "! Jeśli Twój ruch to playerMove, to wygrywasz!"
+  "Zagrałem " +
+    kamie_C5_84 +
+    "! Jeśli Twój ruch to " +
+    playerMove +
+    ", to wygrywasz!"
 );
+
+var randomNumber;
+randomNumber = Math.ceil(Math.random() * 3 + 1);
+printMessage("Wylosowana liczba to: " + randomNumber);
+if (randomNumber == "1") {
+  computerMove = "kamień";
+} else if (randomNumber == "2") {
+  computerMove = "papier";
+} else if (randomNumber == "3") {
+  computerMove = "nożyce";
+} else {
+  computerMove = "nieznany ruch";
+}
+printMessage("Mój ruch: " + computerMove);
